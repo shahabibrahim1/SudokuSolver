dc58cf08244ff79b2f7866a7a66ca06403f22401
Shahab Ibrahimgit config --global user.name Shahab <ishahab84@gmail.com>
2025-04-21 12:33:21 -0600

    reset

--- src/utils/api.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export async function solveSudoku(grid: (number | null)[][]): Promise<number[][]> {
-  const resp = await axios.post('/solve', { grid });
-  return resp.data.grid as number[][];
-}
